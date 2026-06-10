a3f6be3209856f8a8530e63235717cc80254e747
caron2002 <qkrtkddnr122@gmail.com>
2026-06-10 17:53:35 +0900

    First Submit

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@
 #
 # image_name은 다운로드할 Docker 이미지 이름입니다.
 def make_docker_pull_command(image_name):
+    return f"docker pull {image_name}"
     raise NotImplementedError
 
 
@@ -13,6 +14,7 @@ def make_docker_pull_command(image_name):
 #
 # image_name은 실행할 Docker 이미지 이름입니다.
 def make_docker_run_command(image_name):
+    return f"docker run -itd --rm {image_name}"
     raise NotImplementedError
 
 
@@ -29,6 +31,7 @@ def make_port_mapping_command(
     host_port,
     container_port,
 ):
+    return f"docker run -itd --rm -p {host_port}:{container_port} {image_name}"
     raise NotImplementedError
 
 
@@ -45,6 +48,7 @@ def make_volume_mapping_command(
     container_dir,
     image_name,
 ):
+    return f"docker run -v {host_dir}:{container_dir} {image_name}"
     raise NotImplementedError
 
 
@@ -66,4 +70,9 @@ def make_compose_service(
     host_port,
     container_port,
 ):
+    return f"""
+    {service_name}:
+        image: {image_name}
+        ports: {host_port}:{container_port}
+    """
     raise NotImplementedError
